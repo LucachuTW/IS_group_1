f962a61a055244c81511cc3f696153bd7380ea68
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2023-11-16 19:30:19 +0100

    Added walls

--- houseModel.py
+++ houseModel.py
@@ -6,6 +6,7 @@ class HouseModel():
         #@SantiagoRR2004 added the environment.json
         with open("environment.json","r") as file:
             data = json.load(file)
+        self.data = data
         self._availableDrugs: int = data["cabinet"]["numberDrugs"]
 
     def addDrug(self, quantity):
