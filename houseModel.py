4ecba7391b1c5359ee6b95f8fd9cef9b390c62a0
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2023-11-16 19:05:16 +0100

    Revert "Revert "Added environment""
    This reverts commit fedd2f9e95f2798d3e4e24c8dbbc3fe8a3ed6633.

--- houseModel.py
+++ houseModel.py
@@ -1,7 +1,12 @@
+import json
+
 class HouseModel():
     def __init__(self):
         #@Ventupentu
-        self._availableDrugs: int = 0
+        #@SantiagoRR2004 added the environment.json
+        with open("environment.json","r") as file:
+            data = json.load(file)
+        self._availableDrugs: int = data["cabinet"]["numberDrugs"]
 
     def addDrug(self, quantity):
         #@Ventupentu
