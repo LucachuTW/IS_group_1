4a4ac98574fd15d4017634de37a9d8e65d0ce74e
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2023-11-16 18:31:06 +0100

    Drug test

--- tests/test_HouseModel.py
+++ tests/test_HouseModel.py
@@ -4,5 +4,8 @@ import houseModel
 
 class testing(unittest.TestCase):
     def test_env_2(self):
+        # @SantiagoRR2004
         a = houseModel.HouseModel()
-        self.assertEqual(5, 5)
+        number = a.getDrug()
+        a.addDrug(3)
+        self.assertEqual(a.getDrug(), number+3)
