fe2d2eb5d3230141a45639383a906a00f0b29626
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2023-11-17 18:28:10 +0100

    New test

--- tests/test_HouseEnv.py
+++ tests/test_HouseEnv.py
@@ -1,8 +1,11 @@
 import unittest
 import houseEnv
+import houseModel
 
 
 class testing(unittest.TestCase):
-    def test_env_2(self):
-        a = houseEnv.HouseEnv()
-        self.assertEqual(5, 5)
+    def test_correctRelationship(self):
+        # @SantiagoRR2004
+        control = houseModel.HouseModel().getController()
+        control2 = control.getModel().getController()
+        self.assertEqual(control, control2)
