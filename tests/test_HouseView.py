8a8f800837ab73f9dc16b679d8c1c4faf5efd081
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2023-11-17 18:58:33 +0100

    More tests

--- tests/test_HouseView.py
+++ tests/test_HouseView.py
@@ -6,5 +6,17 @@ import houseModel
 class testing(unittest.TestCase):
     def test_createsViewer(self):
         # @SantiagoRR2004
-        control = houseModel.HouseModel().getView()
-        self.assertIsInstance(control, houseView.HouseView)
+        view = houseModel.HouseModel().getView()
+        self.assertIsInstance(view, houseView.HouseView)
+
+    def test_draw(self):
+        # @SantiagoRR2004
+        view = houseModel.HouseModel().getView()
+        drawing = view.draw()
+        self.assertIsInstance(drawing, list)
+
+    def test_drawCabinet(self):
+        # @SantiagoRR2004
+        view = houseModel.HouseModel().getView()
+        drawing = view.drawAgent("Cabinet")
+        self.assertIsInstance(drawing, dict)
