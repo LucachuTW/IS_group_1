1918909c025135cddadc104fb3d363d66e8b960e
antonoterof <150857533+antonoterof@users.noreply.github.com>
2023-11-17 19:46:27 +0100

    Add methods getView, draw and drawAgent

--- houseView.py
+++ houseView.py
@@ -3,3 +3,12 @@ import AbstractHouseView
 class HouseView(AbstractHouseView.AbstractHouseView):
     def __init__(self) -> None:
         pass
+
+    def getView(self):
+        pass
+
+    def draw(self):
+        return []
+
+    def drawAgent(self, cabinet):
+        return {}
