45738dcfcb41dfbb59c8ab7da3ec26f7201d8f81
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2023-11-17 19:12:27 +0100

    Tests about opening and closing

--- tests/test_HouseModel.py
+++ tests/test_HouseModel.py
@@ -23,3 +23,15 @@ class testing(unittest.TestCase):
         model = houseModel.HouseModel()
         status = model.getCabinetStatus()
         self.assertEqual(type(status), bool)
+
+    def test_openCabinet(self):
+        # @SantiagoRR2004
+        model = houseModel.HouseModel()
+        model.setCabinetStatus(True)
+        self.assertEqual(model.getCabinetStatus(), True)
+
+    def test_closeCabinet(self):
+        # @SantiagoRR2004
+        model = houseModel.HouseModel()
+        model.setCabinetStatus(False)
+        self.assertEqual(model.getCabinetStatus(), False)
