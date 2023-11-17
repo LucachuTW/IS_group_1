b9fa5d814737d0b374bd1a41fce43b5b14532a67
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2023-11-17 18:37:43 +0100

    New test

--- tests/test_HouseModel.py
+++ tests/test_HouseModel.py
@@ -8,12 +8,18 @@ class testing(unittest.TestCase):
         a = houseModel.HouseModel()
         number = a.getDrug()
         a.addDrug(3)
-        self.assertEqual(a.getDrug(), number+3)
+        self.assertEqual(a.getDrug(), number + 3)
 
     def test_ChangePosition(self):
         # @SantiagoRR2004
         model = houseModel.HouseModel()
-        oX, oY, dX, dY = 0,0,3,6
-        value = model.getPosition(oX,oY)
+        oX, oY, dX, dY = 0, 0, 3, 6
+        value = model.getPosition(oX, oY)
         model.changePosition(oX, oY, dX, dY)
-        self.assertEqual(value, model.getPosition(dX,dY))
+        self.assertEqual(value, model.getPosition(dX, dY))
+
+    def test_getCabinetStatus(self):
+        # @SantiagoRR2004
+        model = houseModel.HouseModel()
+        status = model.getCabinetStatus()
+        self.assertEqual(type(status), bool)
