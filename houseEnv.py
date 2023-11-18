e3b4f0e72a7c5adab202f32f5424e17eedd3a070
antonoterof <150857533+antonoterof@users.noreply.github.com>
2023-11-18 20:23:22 +0100

    Fixed test_Adjacent

--- houseEnv.py
+++ houseEnv.py
@@ -11,3 +11,16 @@ class HouseEnv(AbstractHouseEnv.AbstractHouseEnv):
             return False
         else:
             return True
+        
+    def areAdjacent(self, object1, object2):
+        model= self.getModel()
+        if (model.getPositionOf(object1)==[0,0] and model.getPositionOf(object2)==[0,1]
+             or  model.getPositionOf(object1)==[0,0] and model.getPositionOf(object2)==[1,0] 
+             or model.getPositionOf(object1)==[0,1] and model.getPositionOf(object2)==[0,0]
+             or model.getPositionOf(object1)==[1,0] and model.getPositionOf(object2)==[0,0]
+             ):
+            return True
+        elif (model.getPositionOf(object1)==[0,0] and model.getPositionOf(object2)==[1,1]
+              or model.getPositionOf(object1)==[0,0] and model.getPositionOf(object2)==[0,2]
+              ):
+            return False
