147d92c58e2ccf6c681fc72a8abef969f04ccd09
antonoterof <150857533+antonoterof@users.noreply.github.com>
2023-11-18 19:59:09 +0100

    Fixed est_cabinetClosed

--- houseEnv.py
+++ houseEnv.py
@@ -3,3 +3,11 @@ import AbstractHouseEnv
 class HouseEnv(AbstractHouseEnv.AbstractHouseEnv):
     def __init__(self) -> None:
         pass
+
+    def addDrug(self, object, quantity):
+        # @antonoterof
+        model= self.getModel()
+        if model.getOpenStatus(object)== False:
+            return False
+        else:
+            model.addDrug(object, quantity)
