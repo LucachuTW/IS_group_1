cd06f92c33d6ee7285a800dd9d6b39d9a8fb9e4a
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2023-11-30 20:04:14 +0100

    Added more docstrings

--- AbstractHouseEnv.py
+++ AbstractHouseEnv.py
@@ -5,26 +5,98 @@ from typing import Any
 class AbstractHouseEnv(ABC):
     @abstractmethod
     def __init__(self, model: Any) -> None:
+        """
+        Initialize the instance with the provided model.
+
+        Args:
+        - model (Any): The model to be associated with the instance.
+
+        Returns:
+            None
+
+        Contributors:
+            - @SantiagoRR2004
+        """
         self.model = model
 
     def setView(self, view: Any) -> None:
-        # @SantiagoRR2004
+        """
+        Set the view for the object.
+
+        This method allows assigning a view object to facilitate
+        communication and interaction with the current object.
+
+        Args:
+        - view (Any): The view object to be set.
+
+        Returns:
+            None
+
+        Contributors:
+        - @SantiagoRR2004
+        """
         self.view = view
 
     def getView(self) -> Any:
-        # @SantiagoRR2004
+        """
+        Get the current view associated with this object.
+
+        Returns:
+            Any: The current view.
+
+        Note:
+            This method returns the view assigned to the object.
+            Ensure that the view has been properly set before calling this method.
+
+        Contributors:
+            - @SantiagoRR2004
+        """
         return self.view
 
     def setModel(self, model: Any) -> None:
-        # @SantiagoRR2004
+        """
+        Set the model for the object.
+
+        This method allows assigning a model object to facilitate
+        communication and interaction with the current object.
+
+        Args:
+        - model (Any): The model object to be set.
+
+        Returns:
+            None
+
+        Contributors:
+        - @SantiagoRR2004
+        """
         self.model = model
 
     def getModel(self) -> Any:
-        # @SantiagoRR2004
+        """
+        Retrieve the model associated with the current instance.
+
+        Returns:
+            Any: The model object.
+
+        Note:
+            This method provides access to the model assigned to the instance.
+            Ensure that the model has been properly set before calling this method.
+
+        Contributors:
+            - @SantiagoRR2004
+        """
         return self.model
 
     def __repr__(self) -> str:
-        # @SantiagoRR2004
+        """
+        Return a string representation of the object suitable for debugging.
+
+        Returns:
+            str: A string representation of the object.
+
+        Contributors:
+            - @SantiagoRR2004
+        """
         attributes = ", ".join(
             f"{key}={value!r}" for key, value in self.__dict__.items()
         )
--- AbstractHouseView.py
+++ AbstractHouseView.py
@@ -5,27 +5,99 @@ from typing import Any
 class AbstractHouseView(ABC):
     @abstractmethod
     def __init__(self, model: Any) -> None:
-        # @SantiagoRR2004
+        """
+        Initialize the instance with the provided model.
+
+        Args:
+        - model (Any): The model to be associated with the instance.
+
+        Returns:
+            None
+
+        Contributors:
+            - @SantiagoRR2004
+        """
         self.model = model
 
     def setController(self, controler: Any) -> None:
-        # @SantiagoRR2004
+        """
+        Set the controller for the object.
+
+        This method allows assigning a controller object to facilitate
+        communication and interaction with the current object.
+
+        Args:
+        - controller (Any): The controller object to be set.
+
+        Returns:
+            None
+
+        Contributors:
+        - @SantiagoRR2004
+        """
         self.controler = controler
 
     def getController(self) -> Any:
-        # @SantiagoRR2004
+        """
+        Retrieve the controller associated with the current instance.
+
+        Returns:
+            Any: The controller object.
+
+        Note:
+            This method provides access to the controller assigned to the instance.
+            Ensure that the controller has been properly set before calling this method.
+
+        Contributors:
+            - @SantiagoRR2004
+        """
         return self.controler
 
     def setModel(self, model: Any) -> None:
-        # @SantiagoRR2004
+        """
+        Set the model for the object.
+
+        This method allows assigning a model object to facilitate
+        communication and interaction with the current object.
+
+        Args:
+        - model (Any): The model object to be set.
+
+        Returns:
+            None
+
+        Contributors:
+        - @SantiagoRR2004
+        """
         self.model = model
 
     def getModel(self) -> Any:
-        # @SantiagoRR2004
+        """
+        Retrieve the model associated with the current instance.
+
+        Returns:
+            Any: The model object.
+
+        Note:
+            This method provides access to the model assigned to the instance.
+            Ensure that the model has been properly set before calling this method.
+
+        Contributors:
+            - @SantiagoRR2004
+        """
         return self.model
 
     def __repr__(self) -> str:
-        # @antonvm2004
+        """
+        Return a string representation of the object suitable for debugging.
+
+        Returns:
+            str: A string representation of the object.
+
+        Contributors:
+            - @SantiagoRR2004
+            - Antón Varela
+        """
         attributes = ", ".join(
             f"{key}={value!r}" for key, value in self.__dict__.items()
         )
