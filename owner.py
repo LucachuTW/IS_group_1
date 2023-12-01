abb6eed9d34f32ae798a98cc8668b1b57fa79ee0
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2023-12-01 12:13:15 +0100

    Added robot and owner

---
+++ owner.py
@@ -0,0 +1,41 @@
+from typing import Any
+
+
+class Owner():
+    def __init__(self, controller: Any, viewer: Any) -> None:
+        self.view = viewer
+        self.controller = controller
+        self.data = viewer.drawAgent("owner")
+
+
+    def getController(self) -> Any:
+        """
+        Retrieve the controller associated with the current instance.
+
+        Returns:
+            Any: The controller object.
+
+        Note:
+            This method provides access to the controller assigned to the instance.
+            Ensure that the controller has been properly set before calling this method.
+
+        Contributors:
+            - @SantiagoRR2004
+        """
+        return self.controller
+
+    def getView(self) -> Any:
+        """
+        Get the current view associated with this object.
+
+        Returns:
+            Any: The current view.
+
+        Note:
+            This method returns the view assigned to the object.
+            Ensure that the view has been properly set before calling this method.
+
+        Contributors:
+            - @SantiagoRR2004
+        """
+        return self.view
