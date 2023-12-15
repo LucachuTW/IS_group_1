cac648aac316780144f78b32864cdd26c234823f
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2023-12-15 20:25:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ import houseModel
 import owner
 import robot
 
-if __name__ == "__main__":
+
+def createHouse():
     model = houseModel.HouseModel()
     controller = model.getController()
     viewer = model.getView()
     owner1 = owner.Owner(controller, viewer)
     robot1 = robot.Robot(controller, viewer)
+
+
+if __name__ == "__main__":
+    createHouse()
